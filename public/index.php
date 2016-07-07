d1be3ff7ed59af0517ddb5e8a034b86cfed904e2
Clay Freeman <git@clayfreeman.com>
2016-07-06 23:14:06 -0500

    rearrange some logic

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
   /**
-   * Temporary entrypoint to the application.
+   * Entrypoint to the application.
    *
    * @copyright Copyright 2016 Clay Freeman. All rights reserved.
    * @license   GNU General Public License v3 (GPL-3.0).
    */
 
   // Load the `Path` class for easier platform-specific path generation
-  $bootstrap = realpath(__DIR__.DIRECTORY_SEPARATOR.'bootstrap.php');
+  $bootstrap = realpath(dirname(__DIR__).DIRECTORY_SEPARATOR.'bootstrap.php');
   (is_file($bootstrap) && is_readable($bootstrap) && include_once($bootstrap))
     or die("Could not load the bootstrap routine.\n");
