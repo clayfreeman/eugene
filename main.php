758721d687e0897365defa022ef32a96995029cb
Clay Freeman <git@clayfreeman.com>
2016-07-11 22:13:42 -0500

    add Registry class sample test

--- main.php
+++ main.php
@@ -6,6 +6,15 @@
    * @license   GNU General Public License v3 (GPL-3.0).
    */
 
-  $r = \Pubkey2\Runtime\Registry::getInstance();
-  $r->create('test', 'Registry class sample test.');
-  echo $r->get('test')."\n";
+  use \Pubkey2\Exceptions\{NameUnavailableError, ReadLockError};
+  use \Pubkey2\Runtime\Registry;
+
+  // Get a reference to the singleton instance of `Registry`
+  $r = Registry::getInstance();
+  // Attempt to create an entry named 'test'
+  try { $r->create('test', 'Registry class sample test.'); }
+  catch (NameUnavailableError $e) {}
+  // Attempt to retrieve the value held by the entry named 'test'
+  try { echo $r->get('test')."\n"; }
+  catch (NameUnavailableError $e) {}
+  catch (ReadLockError $e) {}
