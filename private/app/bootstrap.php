9c651fff1ca26fc9642e5010f709295d594da3aa
Clay Freeman <git@clayfreeman.com>
2016-12-29 10:11:30 -0600

    wip

--- private/app/bootstrap.php
+++ private/app/bootstrap.php
@@ -21,9 +21,9 @@
   define('__DS__',          DIRECTORY_SEPARATOR);
   define('__CLASSPATH__',   realpath(__DIR__.__DS__.'lib'));
   define('__APPROOT__',     realpath(__DIR__));
-  define('__DATAROOT__',    realpath(__APPROOT__.__DS__.'data'));
   define('__PRIVATEROOT__', realpath(dirname(__APPROOT__)));
   define('__CONFIGROOT__',  realpath(__PRIVATEROOT__.__DS__.'config'));
+  define('__DATAROOT__',    realpath(__PRIVATEROOT__.__DS__.'data'));
   define('__PROJECTROOT__', realpath(dirname(__PRIVATEROOT__)));
   define('__PUBLICROOT__',  realpath(__PROJECTROOT__.__DS__.'public'));
 
@@ -36,25 +36,21 @@
     '\'var_export(...)\' function should be disabled using the '.
     '\'disable_functions\' directive', E_USER_WARNING);
 
-  // Define a list of allowed paths during application runtime. Include
-  // directories should be checked for read-only access in addition to this
-  // preventative security measure
-  $allowedDirectories = array_filter([__DATAROOT__, __CLASSPATH__,
-    __PROJECTROOT__.__DS__.'vendor'], function($input) {
-      return is_dir($input) && !stristr($input, PATH_SEPARATOR);
-  }); echo var_export($allowedDirectories, true)."\n";
-  // Restrict file access to prevent unauthorized tampering of application (see
-  // http://php.net/manual/en/ini.core.php#ini.open-basedir for more information
-  // regarding file restriction)
-  ini_set('open_basedir', implode(PATH_SEPARATOR, $allowedDirectories));
-
   // Run the application autoload utility setup file
   require_once(realpath(implode(__DS__,
     [__CLASSPATH__, 'Eugene', 'Utilities', 'Autoload.php'])));
 
+  // Create locally-scoped aliases for the `Path` and`Security` classes
+  use \Eugene\Utilities\{Path, Security};
+
+  // Begin the non-strict lockdown phase of execution (to still allow
+  // configuration file parsing)
+  Security::getInstance()->lockdown();
+
+  use \Eugene\Runtime\Config;
+
   // Scan for project configuration files
-  \Eugene\Runtime\Config::getInstance()->scan();
+  Config::getInstance()->scan();
 
   // Load the composer vendor autoloader to include all composer software
-  require_once(\Eugene\Utilities\Path::make(
-    __PROJECTROOT__, 'vendor', 'autoload.php'));
+  require_once(Path::make(__PROJECTROOT__, 'vendor', 'autoload.php'));
--- private/app/lib/Eugene/Utilities/Security.php
+++ private/app/lib/Eugene/Utilities/Security.php
@@ -14,8 +14,8 @@
   // End script execution if the private root is not defined
   if (!defined('__PRIVATEROOT__')) die();
 
-  // Create a locally-scoped alias for the `Singleton` class
-  use \Eugene\DesignPatterns\Singleton;
+  // Create locally-scoped aliases for the `Singleton` and `Path` classes
+  use \Eugene\{DesignPatterns\Singleton, Utilities\Path};
 
   /**
    * TODO
@@ -42,7 +42,7 @@
      *
      * If the provided file path does not exist, `false` will be returned.
      *
-     * @param   string  $file  The file path to check for mutability.
+     * @param   string  $file  The path to check for mutability.
      *
      * @return  bool           Whether the provided file is mutable.
      */
@@ -50,8 +50,72 @@
       // Check whether the provided file path exists
       if (file_exists($file)) {
         // Check whether the file is writable or is owned by this process
-        return is_writable($file) || fileowner($file) === posix_getuid();
+        return is_writable($file) || fileowner($file) == posix_getuid();
         // TODO: RESTRICT USAGE TO POSIX SYSTEMS
       } return false;
+    }
+
+    /**
+     * Determines whether the provided file path or any subsequent directory
+     * entries are considered mutable
+     *
+     * If the provided file path does not exist, `false` will be returned.
+     *
+     * @see     fileIsMutable()  For more information regarding mutability test.
+     *
+     * @param   string  $file    The path to recursively check for mutability.
+     *
+     * @return  bool             Whether the provided file or any subsequent
+     *                           directory entries are mutable.
+     */
+    public function fileIsRecursivelyMutable(string $file): bool {
+      // Assume that the provided file path is not recursively mutable
+      $result = false;
+      // Ensure that the file exists before continuing
+      if (file_exists($file)) {
+        // Begin by checking the provided file path itself
+        $result = $this->fileIsMutable($file);
+        if ($result === false && is_dir($file)) {
+          // Create a `RecursiveDirectoryIterator` for the provided file path
+          $entries = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($file,
+                \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::SELF_FIRST);
+          // Check the children of the provided file path
+          foreach ($entries as $name => $entry) {
+            // Check this specific child node for mutability
+            $result = $this->fileIsMutable($name);
+            // If the file is mutable, stop looping to save time
+            if ($result === true) break;
+          }
+        } // Return the mutability test results
+      } return $result;
+    }
+
+    /**
+     * [lockdown description]
+     *
+     * @param  bool  $strict  [description]
+     */
+    public function lockdown(bool $strict = false): void {
+      // Define an array of paths that should be allowed if itself and all
+      // children are read-only
+      $ro = [__APPROOT__, Path::make(__PROJECTROOT__, 'vendor')];
+      $rw = [__DATAROOT__];
+      // If operating under strict mode, `__CONFIGROOT__` should be excluded
+      if ($strict === false) $ro[] = __CONFIGROOT__;
+      // Filter the allowed read-only paths to only recursively immutable paths
+      $ro = array_filter($ro, function($input) {
+        return Security::getInstance()->fileIsRecursivelyMutable($input); });
+      // Define a list of allowed paths during application runtime based on the
+      // restricted read-only and read-write paths
+      $allowed = array_filter(array_merge($ro, $rw), function($input) {
+        return is_dir($input) && is_readable($input) &&
+          !stristr($input, PATH_SEPARATOR); });
+      echo var_export($allowed, true)."\n";
+      // Restrict file access to prevent unauthorized tampering of application
+      // (see http://php.net/manual/en/ini.core.php#ini.open-basedir for more
+      // information regarding file restriction)
+      ini_set('open_basedir', implode(PATH_SEPARATOR, $allowed));
     }
   }
