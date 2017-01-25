ae3953e9cdf9fa1acc457e411a51cf5cd10f7636
Clay Freeman <git@clayfreeman.com>
2017-01-25 14:24:08 -0600

    don't check files for read-only access

--- private/app/lib/Eugene/Utilities/Security.php
+++ private/app/lib/Eugene/Utilities/Security.php
@@ -41,13 +41,6 @@
     protected $allowUnlink = false;
 
     /**
-     * TODO
-     *
-     * @var  array
-     */
-    protected $gids        = [];
-
-    /**
      * The symmetric key used for internal password hash encryption.
      *
      * @var  EncryptionKey
@@ -55,22 +48,10 @@
     protected $key         = null;
 
     /**
-     * TODO
-     *
-     * @var  array
-     */
-    protected $uids        = [];
-
-    /**
      * Responsible for generating the symmetric encryption key used by Halite if
      * it doesn't already exist.
      */
     protected function __construct() {
-      // Fetch the remaining supplementary groups for this process
-      $this->gids = array_merge([posix_getegid(), posix_getgid()],
-        posix_getgroups());
-      // Fetch runtime information about the current process
-      $this->uids =             [posix_geteuid(), posix_getuid()];
       // Ensure that Halite is able to work correctly
       Halite::isLibsodiumSetupCorrectly(true) or die();
       // Check if the encryption key exists
@@ -83,68 +64,6 @@
         $this->key = KeyFactory::generateEncryptionKey();
         KeyFactory::save($this->key, $keyPath);
       }
-    }
-
-    /**
-     * TODO
-     *
-     * @param   string  $path  [description]
-     *
-     * @return  array          [description]
-     */
-    protected function fastRecursiveFileEnumerator(string $path): array {
-      // Allocate an array to hold the results (initialized with the given path)
-      $results = [$path];
-      // Get a list of all directory entries for the provided path
-      $scandir = @scandir($path, SCANDIR_SORT_NONE);
-      // Iterate over each directory entry to expand child directories
-      if (is_array($scandir)) foreach ($scandir as $file) {
-        // Skip dot file results to prevent duplicate entries
-        if ($file == '.' || $file == '..') continue;
-        // Convert the relative file name to an absolute file name
-        $file = $path.__DS__.$file;
-        // Expand this path and merge the results
-        $results = array_merge($results,
-          $this->fastRecursiveFileEnumerator($file));
-      // Return the array filled with file paths
-      } return $results;
-    }
-
-    /**
-     * Determines whether the provided file path is considered mutable.
-     *
-     * Mutability is defined as the ability to write to a directory entry
-     * directly or indirectly by using ownership to change file permissions.
-     *
-     * If the provided file path does not exist, `false` will be returned.
-     *
-     * @param   string  $file  The path to check for mutability.
-     *
-     * @return  bool           Whether the provided file is mutable.
-     */
-    public function fileIsMutable(string $file): bool {
-      return     is_writable($file) || // Check if writable using `access(2)`
-        ($owner = @fileowner($file)) === false || // Ensure we can get the owner
-        in_array($owner, $this->uids); // Ensure that we don't own the file
-    }
-
-    /**
-     * Determines whether the provided file path or any subsequent directory
-     * entries are considered mutable
-     *
-     * If the provided file path does not exist, `false` will be returned.
-     *
-     * @see     fileIsMutable()  For more information regarding mutability test.
-     *
-     * @param   string  $file    The path to recursively check for mutability.
-     *
-     * @return  bool             Whether the provided file or any subsequent
-     *                           directory entries are mutable.
-     */
-    public function fileIsRecursivelyMutable(string $file): bool {
-      // Check if any recursive directory entry of the provided path is mutable
-      return count(array_filter(array_map([$this, 'fileIsMutable'],
-        $this->fastRecursiveFileEnumerator($file)))) > 0;
     }
 
     /**
@@ -177,12 +96,6 @@
       $ro = [__APPROOT__, __VENDORROOT__]; $rw = [__DATAROOT__];
       // Include `__CONFIGROOT__` and `__KEYROOT__` in non-strict mode
       if ($strict === false) { $ro[] = __CONFIGROOT__; $rw[] = __KEYROOT__; }
-      // Ensure that only recursively immutable paths are allowed in the
-      // read-only array of paths
-      $ro = array_filter($ro, function($input) {
-        if ($retval = $this->fileIsRecursivelyMutable($input))
-          trigger_error('This path is recursively mutable', E_USER_WARNING);
-        return !$retval; });
       // Define a list of allowed paths during application runtime based on the
       // restricted read-only and read-write paths
       $allowed = array_filter(array_merge($ro, $rw), function($input) {
